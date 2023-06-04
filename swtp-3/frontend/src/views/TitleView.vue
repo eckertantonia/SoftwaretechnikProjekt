4a7652b688c29cc9409d3903fc19734946311007
Antonia Eckert <antonia.eckert@student.hs-rm.de>
2023-06-04 19:03:57 +0200

    title change

--- swtp-3/frontend/src/views/TitleView.vue
+++ swtp-3/frontend/src/views/TitleView.vue
@@ -3,7 +3,7 @@
     class="bg-back-folder-gray bg-blend-overlay h-screen text-white pt-20 flex justify-center items-center bg-[url('/assets/img/bg-menu.png')] bg-center bg-cover"
   >
     <div class="text-center">
-      <h1 class="font-bold text-[10rem] uppercase">Zychose</h1>
+      <h1 class="font-bold text-[10rem] uppercase">Gridline</h1>
       <NameInput />
     </div>
   </div>
